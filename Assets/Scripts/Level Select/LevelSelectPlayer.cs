558cb489ba990dea0d043b63a890e6fbab92852d
MichelCanet <canet.ragot@gmail.com>
2020-03-24 17:41:48 +0100

    Level Select V1

---
+++ Assets/Scripts/Level Select/LevelSelectPlayer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectPlayer : MonoBehaviour
+{
+    public MapPoint currentPoint; //ou se trouve le player
+
+    public float moveSpeed = 10f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, currentPoint.transform.position, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, currentPoint.transform.position) < 0.1f) //Mouvement dans le Level Select
+        {
+            if (Input.GetAxisRaw("Horizontal") > 0.5f) //si le joueur appuie sur les touches "Horizontal droite" alors
+            {
+                if (currentPoint.right != null) //Si la "Right" est possible alors
+                {
+                    SetNextPoint(currentPoint.right);
+                }
+            }
+
+            else if (Input.GetAxisRaw("Horizontal") < -0.5f) //si le joueur appuie sur les touches "Horizontal gauche" alors
+            {
+                if (currentPoint.left != null) //Si la "Left" possible alors
+                {
+                    SetNextPoint(currentPoint.left);
+                }
+            }
+
+            else if (Input.GetAxisRaw("Vertical") > 0.5f) //si le joueur appuie sur les touches "Vertical up" alors
+            {
+                if (currentPoint.up != null) //Si la "Up" est possible alors
+                {
+                    SetNextPoint(currentPoint.up);
+                }
+            }
+
+            else if (Input.GetAxisRaw("Vertical") < -0.5f) //si le joueur appuie sur les touches "Vertical down" alors
+            {
+                if (currentPoint.down != null) //Si la "Down" possible alors
+                {
+                    SetNextPoint(currentPoint.down);
+                }
+            }
+        }       
+    }
+
+    public void SetNextPoint(MapPoint nextPoint)
+    {
+        currentPoint = nextPoint;
+    }
+}
---
+++ Assets/Scripts/Level Select/MapPoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPoint : MonoBehaviour
+{
+    public MapPoint up, right, down, left;
+    public bool isLevel;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
